3fad0079e5b63005b846a7ba003a5e9f2dcd234c
princewill ohuabunwa <wohuabun@gmail.com>
2016-12-08 22:47:58 -0500

    created controllers and corresponding routes

---
+++ app/controllers/Application.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import play.api.mvc._
+
+/**
+  * Created by iWill on 2016-12-08.
+  */
+class Application extends Controller {
+
+  def index = Action {
+    Ok("Your new application is ready.")
+  }
+
+}
---
+++ app/controllers/Widgets.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import play.api.mvc._
+
+/**
+  * Created by iWill on 2016-12-08.
+  */
+
+class Widgets extends Controller {
+
+  def index = TODO
+
+  def create = TODO
+
+  def read(id: String) = TODO
+
+  def update(id: String) = TODO
+
+  def delete(id: String) = TODO
+
+}
